8371c8640bc0aca96af5d83ad86c70d1016087a6
vismodev <vismodeb2000@gmail.com>
2024-03-05 15:43:29 +0600

    Some update

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -4,10 +4,11 @@ import { VscFileMedia } from "react-icons/vsc";
 import { LiaCommentSolid } from "react-icons/lia";
 import { FaUserTie } from "react-icons/fa";
 import { CiSettings } from "react-icons/ci";
+import { RiMenu4Line } from "react-icons/ri";
+import { IoIosNotifications,IoIosSearch } from "react-icons/io";
 
 
 const DashboardLayout = () => {
-
   return (
     <>
       {/* Start: Sidebar */}
@@ -174,7 +175,7 @@ const DashboardLayout = () => {
       <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main">
         <div class="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
           <button type="button" class="text-lg text-gray-600 sidebar-toggle">
-            <i class="ri-menu-line"></i>
+          <RiMenu4Line class="ri-menu-line"/>
           </button>
           <ul class="flex items-center text-sm ml-4">
             <li class="mr-2">
@@ -186,9 +187,9 @@ const DashboardLayout = () => {
           <ul class="ml-auto flex items-center">
               <li class="mr-1 dropdown">
                   <button type="button" class="dropdown-toggle text-gray-400 w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
-                      <i class="ri-search-line"></i>
+                    <IoIosSearch class="ri-search-line"/>
                   </button>
-                  <div class="dropdown-menu shadow-md shadow-black/5 z-30 hidden max-w-xs w-full bg-white rounded-md border border-gray-100">
+                  {/* <div class="dropdown-menu shadow-md shadow-black/5 z-30 hidden max-w-xs w-full bg-white rounded-md border border-gray-100">
                       <form action="" class="p-4 border-b border-b-gray-100">
                           <div class="relative w-full">
                               <input type="text" class="py-2 pr-4 pl-10 bg-gray-50 w-full outline-none border border-gray-100 rounded-md text-sm focus:border-blue-500" placeholder="Search..."/>
@@ -272,13 +273,13 @@ const DashboardLayout = () => {
                               </li>
                           </ul>
                       </div>
-                  </div>
+                  </div> */}
               </li>
               <li class="dropdown">
                   <button type="button" class="dropdown-toggle text-gray-400 w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
-                      <i class="ri-notification-3-line"></i>
+                      <IoIosNotifications class="ri-notification-3-line"/>
                   </button>
-                  <div class="dropdown-menu shadow-md shadow-black/5 z-30 hidden max-w-xs w-full bg-white rounded-md border border-gray-100">
+                  {/* <div class="dropdown-menu shadow-md shadow-black/5 z-30 hidden max-w-xs w-full bg-white rounded-md border border-gray-100">
                       <div class="flex items-center px-4 pt-4 border-b border-b-gray-100 notification-tab">
                           <button type="button" data-tab="notification" data-tab-page="notifications" class="text-gray-400 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1 active">Notifications</button>
                           <button type="button" data-tab="notification" data-tab-page="messages" class="text-gray-400 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1">Messages</button>
@@ -379,22 +380,22 @@ const DashboardLayout = () => {
                               </li>
                           </ul>
                       </div>
-                  </div>
+                  </div> */}
               </li>
               <li class="dropdown ml-3">
                   <button type="button" class="dropdown-toggle flex items-center">
-                      <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle"/>
+                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle"/>
                   </button>
                   <ul class="dropdown-menu shadow-md shadow-black/5 z-30 hidden py-1.5 rounded-md bg-white border border-gray-100 w-full max-w-[140px]">
-                      <li>
-                          <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Profile</a>
-                      </li>
-                      <li>
-                          <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Settings</a>
-                      </li>
-                      <li>
-                          <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Logout</a>
-                      </li>
+                    <li>
+                      <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Profile</a>
+                    </li>
+                    <li>
+                      <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Settings</a>
+                    </li>
+                    <li>
+                      <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Logout</a>
+                    </li>
                   </ul>
               </li>
           </ul>
