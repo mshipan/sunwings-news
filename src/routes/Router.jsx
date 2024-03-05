986670c6f6b4c253155d621c4c33b9191f39dc99
vismodev <vismodeb2000@gmail.com>
2024-03-05 19:34:16 +0600

    Main Layout add

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
+import MainLayout from "../layouts/MainLayout";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "test",
+        element: <h1>Hello</h1>,
+      },
+    ],
+  },
   {
     path: "/dashboard",
     element: <DashboardLayout />,
