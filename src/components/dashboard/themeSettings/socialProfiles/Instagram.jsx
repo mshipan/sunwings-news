c5b3afe1a8ee882d7ea0158a83970ccac4f9b012
Shipan Mallik <mshipan657@gmail.com>
2024-03-16 16:12:44 +0600

    create instagram and youtube api

--- src/components/dashboard/themeSettings/socialProfiles/Instagram.jsx
+++ src/components/dashboard/themeSettings/socialProfiles/Instagram.jsx
@@ -1,22 +1,21 @@
 import { useForm } from "react-hook-form";
-
 import { useState } from "react";
 import { imageUpload } from "../../../../api/utils";
 import Swal from "sweetalert2";
-import {
-  useGetAllFacebookQuery,
-  useUpdateFacebookMutation,
-} from "../../../../redux/features/allApis/socialMediaApi/facebookApi";
 import { Link } from "react-router-dom";
+import {
+  useGetAllInstagramQuery,
+  useUpdateInstagramMutation,
+} from "../../../../redux/features/allApis/socialMediaApi/instagramApi";
 
-const Facebook = () => {
+const Instagram = () => {
   const [loading, setLoading] = useState(false);
   const { register, handleSubmit, watch, reset } = useForm();
 
-  const { data: allFacebooks } = useGetAllFacebookQuery();
-  const [updateFacebook] = useUpdateFacebookMutation();
-  const id = allFacebooks?.[0]._id;
-  const singleFacebook = allFacebooks?.[0];
+  const { data: allInstagram } = useGetAllInstagramQuery();
+  const [updateInstagram] = useUpdateInstagramMutation();
+  const id = allInstagram?.[0]._id;
+  const singleInstagram = allInstagram?.[0];
   const onSubmit = async (data) => {
     const profileImage = watch("profilePhoto");
     const coverImage = watch("coverPhoto");
@@ -26,7 +25,7 @@ const Facebook = () => {
       const imageData2 = await imageUpload(coverImage[0]);
       data.profilePhoto = imageData1.data.display_url;
       data.coverPhoto = imageData2.data.display_url;
-      const result = await updateFacebook({
+      const result = await updateInstagram({
         id: id,
         data: data,
       });
@@ -34,7 +33,7 @@ const Facebook = () => {
         setLoading(false);
         reset();
         Swal.fire({
-          title: "Facebook Updated Successfully!",
+          title: "Instagram Updated Successfully!",
           text: "Press OK to continue",
           icon: "success",
           confirmButtonText: "OK",
@@ -45,7 +44,7 @@ const Facebook = () => {
       Swal.fire({
         position: "center",
         icon: "error",
-        title: `Error Updating Facebook: ${error}`,
+        title: `Error Updating Instagram: ${error}`,
         showConfirmButton: false,
         timer: 1500,
       });
@@ -53,7 +52,7 @@ const Facebook = () => {
   };
   return (
     <div>
-      <h1 className="text-black text-xl mb-2">Facebook</h1>
+      <h1 className="text-black text-xl mb-2">Instagram</h1>
       <div className="flex flex-col md:flex-row gap-6">
         <form
           onSubmit={handleSubmit(onSubmit)}
@@ -61,53 +60,51 @@ const Facebook = () => {
         >
           <div className="form-control">
             <label htmlFor="title" className="text-black mb-2 text-lg">
-              Page Title:
+              Instagram Id Title:
             </label>
             <input
               type="text"
               name="title"
               {...register("title")}
               className="bg-white border border-gray-400 px-3 py-2 text-black"
-              placeholder="Enter Page Title..."
+              placeholder="Enter Title..."
             />
           </div>
 
           <div className="form-control">
             <label htmlFor="link" className="text-black mb-2 text-lg">
-              Page Link:
+              Id Link:
             </label>
             <input
               type="text"
               name="link"
               {...register("link")}
               className="bg-white border border-gray-400 px-3 py-2 text-black"
-              placeholder="Enter Page Link..."
+              placeholder="Enter Link..."
             />
           </div>
 
           <div className="form-control">
             <label htmlFor="profilePhoto" className="text-black mb-2 text-lg">
-              Page Profile Photo:
+              Profile Photo:
             </label>
             <input
               type="file"
               name="profilePhoto"
               {...register("profilePhoto")}
               className="bg-white border border-gray-400 px-3 py-2 text-black"
-              placeholder="Enter Page Link..."
             />
           </div>
 
           <div className="form-control">
             <label htmlFor="coverPhoto" className="text-black mb-2 text-lg">
-              Page Cover Photo:
+              Cover Photo:
             </label>
             <input
               type="file"
               name="coverPhoto"
               {...register("coverPhoto")}
               className="bg-white border border-gray-400 px-3 py-2 text-black"
-              placeholder="Enter Page Link..."
             />
           </div>
 
@@ -121,32 +118,32 @@ const Facebook = () => {
                 Updating...
               </div>
             ) : (
-              "Update Facebook"
+              "Update Instagram"
             )}
           </button>
         </form>
-        {singleFacebook && (
+        {singleInstagram && (
           <div className="md:w-1/2 h-fit flex flex-col gap-3 border border-gray-500">
             <div className="relative">
               <div>
                 <img
-                  src={singleFacebook?.coverPhoto}
+                  src={singleInstagram?.coverPhoto}
                   alt=""
                   className="w-full"
                 />
               </div>
               <div className="absolute bottom-0 md:-bottom-7 left-4">
                 <img
-                  src={singleFacebook?.profilePhoto}
+                  src={singleInstagram?.profilePhoto}
                   alt=""
                   className="md:size-48 size-32 rounded-full border border-gray-500"
                 />
               </div>
               <div className="flex flex-col gap-1 p-4 absolute bottom-2 md:bottom-6 left-28 md:left-48 ">
                 <h1 className=" text-xl md:text-4xl text-white font-medium leading-none">
-                  <Link to={singleFacebook?.link}>
+                  <Link to={singleInstagram?.link}>
                     <span className="ml-5 hover:underline">
-                      {singleFacebook?.title}
+                      {singleInstagram?.title}
                     </span>
                   </Link>
                 </h1>
@@ -159,4 +156,4 @@ const Facebook = () => {
   );
 };
 
-export default Facebook;
+export default Instagram;
