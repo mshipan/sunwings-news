30b0bb723be896d180dfcd135809b27623e43e72
Shipan Mallik <mshipan657@gmail.com>
2024-03-25 19:33:28 +0600

    make the footer theme color dynamic

--- src/components/shared/FooterCopyright.jsx
+++ src/components/shared/FooterCopyright.jsx
@@ -4,10 +4,20 @@ import { useGetFooterQuery } from "../../redux/features/allApis/footerApi/footer
 const FooterCopyright = () => {
   const { data: allFooters } = useGetFooterQuery();
   const singleFooter = allFooters?.[0];
+  const dynamicBgColorStyle = singleFooter
+    ? { backgroundColor: singleFooter.copyrightBgColor }
+    : { backgroundColor: "#454444" };
+
+  const dynamicTextColorStyle = singleFooter
+    ? { color: singleFooter.copyrightTextColor }
+    : { color: "#ffff" };
 
   const currentYear = new Date().getFullYear();
   return (
-    <div className="container py-6 px-2 mx-auto bg-slate-800">
+    <div
+      className="container py-6 px-2 mx-auto"
+      style={{ ...dynamicBgColorStyle, ...dynamicTextColorStyle }}
+    >
       <div className="text-center flex flex-row gap-1 justify-center">
         <span>© {currentYear} Copyright:</span>
         <Link className="font-semibold" to={singleFooter?.copyrightLink}>
