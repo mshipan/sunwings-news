0b7106fc0e2220ed6e1337fbdc23648d5a307c60
simanto97 <saimumsimanto97@gmail.com>
2024-04-03 15:00:20 +0600

    modified

--- src/components/dashboard/Topbar.jsx
+++ src/components/dashboard/Topbar.jsx
@@ -2,8 +2,11 @@ import { IoIosNotifications, IoIosSearch } from "react-icons/io";
 import { RiMenu4Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import BreadCrumbs from "../shared/BreadCrumbs";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Topbar = ({ toggleSidebar }) => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
       <button
@@ -34,9 +37,10 @@ const Topbar = ({ toggleSidebar }) => {
         <li className="dropdown ml-3">
           <button type="button" className="dropdown-toggle flex items-center">
             <img
-              src="https:placehold.co/32x32"
-              alt=""
-              className="w-8 h-8 rounded block object-cover align-middle"
+              src={`${user?.photoURL}`}
+              alt="User Image"
+              className="w-8 h-8 rounded-full block object-cover align-middle"
+              title="User Image"
             />
           </button>
           <ul className="dropdown-menu shadow-md shadow-black/5 z-30 hidden py-1.5 rounded-md bg-white border border-gray-100 w-full max-w-[140px]">
