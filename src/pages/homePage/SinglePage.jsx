925b3977b460feab1b9f77ded21543cf4335ef77
Shipan Mallik <mshipan657@gmail.com>
2024-04-17 18:54:00 +0600

    use share functions on single post page

--- src/pages/homePage/SinglePage.jsx
+++ src/pages/homePage/SinglePage.jsx
@@ -10,6 +10,14 @@ import CommentSection from "./CommentSection";
 import { useGetBodyThemeQuery } from "../../redux/features/allApis/bodyThemeApi/bodyThemeApi";
 import { FaPrint } from "react-icons/fa6";
 import Loader from "../../components/shared/Loader/Loader";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "react-share";
 
 const SinglePage = () => {
   const { id } = useParams();
@@ -87,10 +95,32 @@ const SinglePage = () => {
         </p>
 
         <p dangerouslySetInnerHTML={{ __html: styledRenderContent }}></p>
-        <div onClick={handlePrintButton} className="inline-block">
-          <div className="my-2 flex flex-row gap-2 items-center justify-center bg-blue-700 text-white text-xl px-4 py-2">
-            <span>প্রিন্ট করুন : </span>
-            <FaPrint />
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <div onClick={handlePrintButton} className="inline-block">
+            <div className="my-2 flex flex-row gap-2 items-center justify-center bg-blue-700 text-white text-xl px-4 py-2">
+              <span>প্রিন্ট করুন : </span>
+              <FaPrint />
+            </div>
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <div>
+              <p className="text-lg text-black">Share To: </p>
+            </div>
+            <FacebookShareButton
+              url={`${import.meta.env.VITE_SITE_URL}/posts/${id}`}
+            >
+              <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+            <TwitterShareButton
+              url={`${import.meta.env.VITE_SITE_URL}/posts/${id}`}
+            >
+              <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+            <LinkedinShareButton
+              url={`${import.meta.env.VITE_SITE_URL}/posts/${id}`}
+            >
+              <LinkedinIcon size={32} round={true} />
+            </LinkedinShareButton>
           </div>
         </div>
         <CommentSection
